92f8e86ce1afb7873164f6cc46ee29807057c268
yaqzaan <97394346+yaqzaan@users.noreply.github.com>
2023-03-19 19:55:02 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+import "./styles.css";
+
+function App() {
+
+  const list = [
+    "Banana",
+    "Apple",
+    "Orange",
+    "Mango",
+    "Pineapple",
+    "Watermelon"
+  ];
+
+  const [filterList, setFilterList] = useState(list);
+
+  const handleSearch = (event) => {
+
+    if (event.target.value === "") {
+      setFilterList(list);
+      return;
+    }
+    const filteredValues = list.filter(
+      (item) =>
+        item.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1
+    );
+    setFilterList(filteredValues);
+  };
+  return (
+    <div className="app">
+      <div>
+        Search: <input name="query" type="text" onChange={handleSearch} />
+      </div>
+      {filterList &&
+        filterList.map((item, index) => (
+          <div key={index}>{item}</div> //Display each item
+        ))}
+    </div>
+  );
+}
+
+export default App;
